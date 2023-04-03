a448707420fcb2d7d0a4e9da71fa9baf5fb1e54b
niekky <trungkiencfc@gmail.com>
2023-04-02 18:40:49 -0700

    Deleted a corrupted midi file

--- decoder/symphony_net/src/fairseq/linear_transformer/linear_transformer_multi.py
+++ decoder/symphony_net/src/fairseq/linear_transformer/linear_transformer_multi.py
@@ -266,11 +266,11 @@ class LinearTransformerMultiHeadDecoder(FairseqDecoder):
         x,
         src_lengths = None
     ):
-        print("Input: ", x)
-        print("Input torch.max: ", torch.max(x, dim=0))
-        print("Input Size: ", x.size())
-        print("Input a: ",x[..., 0])
-        print("Input a size: ", x[..., 0].size())
+        # print("Input: ", x)
+        # print("Input torch.max: ", torch.max(x, dim=0))
+        # print("Input Size: ", x.size())
+        # print("Input a: ",x[..., 0])
+        # print("Input a size: ", x[..., 0].size())
         bsz, seq_len, ratio = x.size()
         
         evt_emb = self.wEvte(x[..., 0])
