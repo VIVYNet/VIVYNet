df5ecff8b54781e25b08045e4ca13cc721df78be
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-06-09 18:51:25 -0700

    Added a VE portion of the model

---
+++ vivynet/train_ve.sh
@@ -0,0 +1,31 @@
+fairseq-train ../data/final \
+  --user-dir ./ \
+  --task text2music_ve \
+  --criterion nll_loss_ve \
+  --arch vivy_train_ve \
+  --optimizer adam \
+  --lr 0.001 \
+  --max-tokens 4096 \
+  --shorten_method none \
+  --shorten_data_split_list '' \
+  --tokens_per_sample 4096 \
+  --seed 1998 \
+  --sample_break_mode complete_doc \
+  --ratio 4 \
+  --sample_overlap_rate 4 \
+  --perm_inv 2 \
+  --evt_voc_size 1125 \
+  --trk_voc_size 44 \
+  --dur_voc_size 36 \
+  --ins_voc_size 133 \
+  --max_rel_pos  198 \
+  --max_mea_pos  5360 \
+  --batch-size 2 \
+  --freeze_enc 1 \
+  --freeze_dec 1
+  # --evt_voc_size 436 \
+  # --trk_voc_size 44 \
+  # --dur_voc_size 36 \
+  # --ins_voc_size 84 \
+  # --max_rel_pos  134 \
+  # --max_mea_pos  2810 \
