3e1cdbf05e0cdb337d3f8c61af7e6a55d6b21a33
[ Benjamin Joseph Herrera - ENGR Academic Blherre4 Student Affrs - Undergraduate - yyang305 ] <blherre4@agave1.agave.rc.asu.edu>
2023-06-19 00:29:39 -0700

    Shorten Changes

--- vivynet/VIVYNet.py
+++ vivynet/VIVYNet.py
@@ -1379,19 +1379,23 @@ class VIVYData(LanguageModelingTask):
 
         # Data shortening for debugging
         short_src = []
+        short_src_vocab = []
         short_tgt = []
+        short_tgt_vocab = []
         for i in range(20):
             short_src.append(src_dataset[i])
+            short_src_vocab.append(src_dataset.sizes[i])
             short_tgt.append(final_target[i])
+            short_tgt_vocab.append(final_target.sizes[i])
         VIVYData.debug.ldf("DEBUG - SHORTENING")
 
         # Data compilation
         self.datasets[split] = PairDataset(
             src=short_src or src_dataset,
-            src_sizes=src_dataset.sizes,
+            src_sizes=short_src_vocab or src_dataset.sizes,
             src_dict=self.src_vocab,
             tgt=short_tgt or final_target,
-            tgt_sizes=final_target.sizes,
+            tgt_sizes=short_tgt_vocab or final_target.sizes,
             tgt_dict=self.tgt_vocab,
         )
         VIVYData.debug.ldf("COMPILATION")
